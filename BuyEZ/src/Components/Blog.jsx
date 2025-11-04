544ab17c2f57d9b8a757d07ee91d0a43912d41ac
vibo777 <54715728+vibo777@users.noreply.github.com>
2025-11-04 11:23:27 +0530

    Update

--- BuyEZ/src/Components/Blog.jsx
+++ BuyEZ/src/Components/Blog.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 export default function Blog() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://loxam.re/wp-json/wp/v2/posts")
+      .then(res => res.json())
+      .then(data => 
+        setPosts(data)
+      )
+      .catch(err => console.error("Error fetching posts:", err));
+  }, []);
+
   return (
     <div>
-      <h1>Welcome to Blog Page !!!</h1>
+      <h2>WordPress Blog Posts</h2>
+      {posts.map(post => (
+        <div key={post.id}>
+          <h2>{post.title.rendered}</h2>
+          <h3>{post.excerpt.rendered}</h3>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
+
